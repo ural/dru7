f4fc45b527a817a9d1d1edd9762b7fe326357af8
ural <sevomax@gmail.com>
2017-01-03 21:24:57 -0500

    added zurb-foundation themes, added compass to ninesixtyrobots theme

---
+++ sites/all/themes/bison/template.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Implements template_preprocess_html().
+ */
+function bison_preprocess_html(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_page.
+ */
+function bison_preprocess_page(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_node.
+ */
+function bison_preprocess_node(&$variables) {
+}
