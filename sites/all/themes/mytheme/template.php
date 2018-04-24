6f9973608c62eb6a89d212527fb90f9c2499ba38
ural <sevomax@gmail.com>
2018-04-23 21:21:53 -0400

    added mytheme custom theme

---
+++ sites/all/themes/mytheme/template.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Implement hook_preprocess_page()
+ */
+
+/*function mytheme_preprocess_page(&$vars) {
+  dpm($vars);
+}*/
+
+/**
+ * Add custom PHPTemplate variables into the node template.
+ */
+/*function mytheme_preprocess_node(&$vars) {
+  dpm($vars);
+}*/
