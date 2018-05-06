b7a8d4baf42ba16393e934127087761019976925
ural <sevomax@gmail.com>
2018-05-06 17:19:55 -0400

    tried again to install with [npm install], grunt still fails

--- sites/all/themes/bison/template.php
+++ sites/all/themes/bison/template.php
@@ -10,7 +10,7 @@ function bison_preprocess_html(&$variables) {
  * Implements template_preprocess_page.
  */
 function bison_preprocess_page(&$variables) {
-  dpm($variables);
+ // dpm($variables);
 }
 
 /**
