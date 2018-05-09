ff55870fba133d2e6af8f0a6c46c3d9d40230121
ural <sevomax@gmail.com>
2018-05-08 20:53:19 -0400

    deleted bison sub-theme and created zubr sub-theme off of foundation

---
+++ sites/all/themes/zubr/template.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Implements template_preprocess_html().
+ */
+function zubr_preprocess_html(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_page.
+ */
+function zubr_preprocess_page(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_node.
+ */
+function zubr_preprocess_node(&$variables) {
+}
